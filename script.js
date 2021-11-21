4e21ad25b698fedeb1fac78c55d8819b97614527
JDC1492 <Jonathan_Cohen10@yahoo.com>
2021-11-21 15:18:17 -0500

    added all the files and started writing out some js.

---
+++ script.js
@@ -0,0 +1,11 @@
+let crdDsp = document.getElementById("carDisplay");
+let textArea = document.getElementById("txtArea");
+let btnArea = document.getElementById("btnArea");
+let shuffleButton = document.getElementById("shuffle");
+let drawButton = document.getElementById("draw");
+
+shuffleButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  debugger;
+  console.log("You remember!");
+});
